9c63f224fe05b705c3cd7838a2f319398f53d8c0
x-Arte <zmyArte010828>
2024-07-12 21:50:40 +0100

    7/12/2024 function done

--- models/Trigger.py
+++ models/Trigger.py
@@ -1,6 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from . import db
 
+
 class Trigger(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(255), unique=True, nullable=False)
